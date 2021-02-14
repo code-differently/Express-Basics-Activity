1d48b0263771e87b535ea2143459bcc24a127222
Nick Satinover <nsatinover@gmail.com>
2021-02-14 13:39:05 -0500

    express basics activity cleanup

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.engine('pug', require('pug').__express)
-//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -19,20 +18,16 @@ app.get('/', (req, res) => {
 app.post('/deposit', (req, res) => {
     const transaction = parseInt(req.body.transaction);
     const balance = parseInt(req.body.balance);
-    console.log(transaction);
-    console.log(balance);
     res.render('layout', {balance: balance + transaction});
 });
 
 app.post('/withdraw', (req, res) => {
     const transaction = parseInt(req.body.transaction);
     const balance = parseInt(req.body.balance);
-    console.log(transaction);
-    console.log(balance);
     res.render('layout', {balance: balance - transaction});
 });
 
 
 app.listen(3000, () => {
     console.log(`running on localhost:3000`);
-}); // http://localhost:3001/
+});
